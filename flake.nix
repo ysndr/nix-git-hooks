45cc26b275b8aef6b5b8c4183816aa9b77017d69
Yannik Sander <me@ysndr.de>
2021-12-05 18:51:47 +0100

    Use `nixpkgs-fmt` instead of `nixfmt`

--- flake.nix
+++ flake.nix
@@ -3,27 +3,28 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-
-
-
   outputs = { self, nixpkgs, flake-utils }:
     {
       lib.mkInstaller = pkgs: hookTypes:
         let
           mkHook = type: hooks: {
-            hook = pkgs.writeShellScript type
-              ''
-                for hook in ${pkgs.symlinkJoin { name = "${type}-git-hooks"; paths = hooks; }}/bin/*; do
-                $hook
-                RESULT=$?
-                if [ $RESULT != 0 ]; then
-                    echo "$hook returned non-zero: $RESULT, abort operation"
-                exit $RESULT
-                fi
-                done
-                echo "$INSTALLED_GIT_HOOKS $type"
-                exit 0
-              '';
+            hook = pkgs.writeShellScript type ''
+              for hook in ${
+                pkgs.symlinkJoin {
+                  name = "${type}-git-hooks";
+                  paths = hooks;
+                }
+              }/bin/*; do
+              $hook
+              RESULT=$?
+              if [ $RESULT != 0 ]; then
+                  echo "$hook returned non-zero: $RESULT, abort operation"
+              exit $RESULT
+              fi
+              done
+              echo "$INSTALLED_GIT_HOOKS $type"
+              exit 0
+            '';
             inherit type;
           };
 
@@ -38,29 +39,28 @@
 
           uninstaller = self.lib.mkUninstaller pkgs;
         in
-        pkgs.writeShellScriptBin "install-git-hooks"
-          ''
-            if [[ ! -d .git ]] || [[ ! -f flake.nix ]]; then
-                echo "Invocate \`nix develop\` from the project root directory."
-                exit 1
-            fi
+        pkgs.writeShellScriptBin "install-git-hooks" ''
+          if [[ ! -d .git ]] || [[ ! -f flake.nix ]]; then
+              echo "Invocate \`nix develop\` from the project root directory."
+              exit 1
+          fi
 
-            if [[ -e .git/hooks/nix-installed-hooks ]]; then
-                echo "Hooks already installed, reinstalling"
-                ${uninstaller}/bin/${uninstaller.name}
-            fi
+          if [[ -e .git/hooks/nix-installed-hooks ]]; then
+              echo "Hooks already installed, reinstalling"
+              ${uninstaller}/bin/${uninstaller.name}
+          fi
 
-            mkdir -p ./.git/hooks
-        
-            ${pkgs.lib.concatStringsSep "\n" (nixpkgs.lib.mapAttrsToList (type: hooks: installHookScript (mkHook type hooks)) hookTypes )}
+          mkdir -p ./.git/hooks
 
-            echo "Installed git hooks: $INSTALLED_GIT_HOOKS"
-            printf "%s\n" "''${INSTALLED_GIT_HOOKS[@]}" > .git/hooks/nix-installed-hooks
-          '';
+          ${pkgs.lib.concatStringsSep "\n" (nixpkgs.lib.mapAttrsToList
+            (type: hooks: installHookScript (mkHook type hooks)) hookTypes)}
 
+          echo "Installed git hooks: $INSTALLED_GIT_HOOKS"
+          printf "%s\n" "''${INSTALLED_GIT_HOOKS[@]}" > .git/hooks/nix-installed-hooks
+        '';
 
-      lib.mkUninstaller = pkgs: pkgs.writeShellScriptBin "uninstall-git-hooks"
-        ''
+      lib.mkUninstaller = pkgs:
+        pkgs.writeShellScriptBin "uninstall-git-hooks" ''
           if [[ ! -e "$PWD/.git/hooks/nix-installed-hooks" ]]; then
           echo "Error: could find list of installed hooks."
           exit 1
@@ -80,9 +80,7 @@
         git-hook-uninstaller = self.lib.mkUninstaller prev;
       };
 
-
-    } //
-    flake-utils.lib.eachDefaultSystem (system:
+    } // flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -94,14 +92,14 @@
         devShell =
           let
             nixFormatHook = pkgs.writeShellScriptBin "check-rust-format-hook" ''
-              ${pkgs.nixfmt}/bin/nixfmt --check
+              ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check flake.nix
               RESULT=$?
-              [ $RESULT != 0 ] && echo "Please run \`nixfmt\` before committing"
+              [ $RESULT != 0 ] && echo "Please run \`nixpkgs-fmt\` before committing"
               exit $RESULT
             '';
 
-            hookInstaller = pkgs.git-hook-installer { pre-commit = [ nixFormatHook ]; };
-
+            hookInstaller =
+              pkgs.git-hook-installer { pre-commit = [ nixFormatHook ]; };
           in
           pkgs.mkShell {
             packages = [ hookInstaller pkgs.git-hook-uninstaller ];
